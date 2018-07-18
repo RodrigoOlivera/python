df2a6c2fb580e3d236c2b40404bfc0f73079fe15
Rodrigo Olivera <rodrigoolivera27@yahoo.com.ar>
2018-07-18 16:25:39 -0300

    all

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Response
+import json
 
 app = Flask(__name__)
 
@@ -40,10 +41,57 @@ def hello_world():
 def add_book():
     request_data = request.get_json()
     if (validBookObject(request_data)):
-        books.insert(0, request_data)
-        return "True"
+        new_book = {
+            'name': request_data["name"],
+            'price': request_data["price"],
+            'isbn': request_data["isbn"]
+        }
+        books.insert(0, new_book)
+        response = Response("",201, mimetype='application/json')
+        response.headers['Location'] = "/books" + str(new_book["isbn"])
+        return response
     else:
-        return "False"
+        invalidBookObjectErrorMsg = {
+            "error": "U suck",
+            "helpString": "Suck less"
+        }
+        response = Response(json.dumps(invalidBookObjectErrorMsg), status=400, mimetype='application/json');
+        return response
+
+#PUT /books
+@app.route('/books/<int:isbn>', methods=['PUT'])
+def replace_book(isbn):
+    request_data = request.get_json()
+    new_book = {
+            'name': request_data['name'],
+            'price': request_data['price'],
+            'isbn': isbn
+    }
+    i = 0;
+    for book in books:
+        currentIsbn = book["isbn"]
+        if currentIsbn == isbn:
+            books[i] = new_book
+        i += 1
+    response = Response("", status=204)
+    return response
+
+#PATCH /books
+@app.route('/books/<int:isbn>', methods=['PATCH'])
+def update_book(isbn):
+    request_data = request.get_json()
+    updated_book = {}
+    if ("name" in request_data):
+        updated_book["name"] = request_data['name']
+    if ("price" in request_data):
+        updated_book["price"] = request_data['price']
+    for book in books:
+        if book["isbn"] == isbn:
+            book.update(updated_book)
+    response = Response("", status=204)
+    response.headers['Location'] = '/books/' + str(isbn)
+    return response
+
 
 #/int:isbn is a cast of the variable isbn to int
 @app.route('/books/<int:isbn>')
